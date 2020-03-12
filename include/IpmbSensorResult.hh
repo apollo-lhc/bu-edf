d2fa9069515a3968d4ed64fd109d862de1dedab6
bkilian15 <bkilian@bu.edu>
2020-03-12 08:54:56 -0400

    Added gitignore. Added missing file

---
+++ include/IpmbSensorResult.hh
@@ -0,0 +1,13 @@
+#ifndef __IPMB_SENSOR_RESULT_HH__
+#define __IPMB_SENSOR_RESULT_HH__
+#include <stdint.h>
+
+
+struct IpmbSensorResult {
+  uint8_t empty_byte1;
+  uint8_t empty_byte22;
+  uint8_t sensorValue;
+  uint8_t status;
+  uint8_t mask;
+};
+#endif
