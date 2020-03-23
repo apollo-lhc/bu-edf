733310134284698f4c1d72986d6acefd58fd07f6
bkilian15 <bkilian@bu.edu>
2020-03-23 14:47:30 -0400

    ApolloReader can now read out 248 bytes of data and read the header to determine offsets

--- include/ApolloReader.hh
+++ include/ApolloReader.hh
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <freeipmi/api/ipmi-api.h>
 
 class ApolloReader {
 
@@ -15,16 +16,32 @@ public:
   int GetProductSerial();
 
   std::string GetProductName();
+
+  void Read();
   
 private:
 
-  void Read();
+  void ReadHeader(ipmi_ctx_t ipmiContext);
+  
+  int ReadInformationLength(ipmi_ctx_t ipmiContext);
 
+  int informationLength;
+  
   char *hostname;
   
   uint8_t deviceAccessAddress;
   
   std::vector<uint8_t> header;
+  // fields for data stored in header
+  uint8_t headerFormatVersion;
+  uint8_t internalUseStartingOffset;
+  uint8_t chassisInfoStartingOffset;
+  uint8_t boardStartingOffset;
+  uint8_t productInfoStartingOffset;
+  uint8_t multiRecordStartingOffset;
+  uint8_t pad;
+  uint8_t commonHeaderChecksum;
+  
   
   std::vector<uint8_t> data;
 
--- src/ApolloReader.cc
+++ src/ApolloReader.cc
@@ -3,6 +3,8 @@
 #include <freeipmi/api/ipmi-api.h>
 #include <freeipmi/spec/ipmi-authentication-type-spec.h>
 #include <freeipmi/spec/ipmi-privilege-level-spec.h>
+#include <stdexcept>
+#include <string.h>
 
 ApolloReader::ApolloReader(char *hostname_, uint8_t deviceAddr){
   hostname = hostname_;
@@ -15,6 +17,63 @@ ApolloReader::ApolloReader(char *hostname_, uint8_t deviceAddr){
 */
 
 
+int ApolloReader::ReadInformationLength(ipmi_ctx_t ipmiContext) {
+  
+  uint8_t channel_number = 0;
+  uint8_t lun = 0;
+  uint8_t net_fn = 0xa; // 0xa is net fn for storage
+  const size_t buf_rq_size = 2;
+
+  uint8_t buf_rq[buf_rq_size];
+
+  uint8_t read_code = 0x10;
+  uint8_t fru_id = 0;
+
+  buf_rq[0] = read_code;
+  buf_rq[1] = fru_id;
+
+
+  const size_t buf_rs_size = 256;
+  uint8_t buf_rs[buf_rs_size];
+
+  int raw_result = ipmi_cmd_raw_ipmb(ipmiContext,
+				     channel_number,
+				     deviceAccessAddress,
+				     lun,
+				     net_fn,
+				     (void const *) buf_rq, buf_rq_size,
+			             buf_rs, buf_rs_size);
+
+  uint8_t length_ls_byte = buf_rs[2];
+  uint8_t length_ms_byte = buf_rs[3];
+
+  informationLength = 256*length_ms_byte + length_ls_byte;
+  printf("information length: %d\n", informationLength);
+  
+  for(int i = 0; i < 5; i++){
+    printf("LENGTH: %d ", buf_rs[i]);
+  }
+  printf("\n");
+}
+
+
+
+
+void ApolloReader::ReadHeader(ipmi_ctx_t ipmiContext){
+  // read the first 8 bytes of data, figure out offsets, how many bytes are used
+}
+
+
+
+
+
+
+
+
+
+
+
+
 void ApolloReader::Read(){
 
   ipmi_ctx_t ipmiContext_ = ipmi_ctx_create();
@@ -22,7 +81,7 @@ void ApolloReader::Read(){
   char *hostname = "192.168.10.171";
 
   int connection = ipmi_ctx_open_outofband(ipmiContext_,
-					   hostnme,
+					   hostname,
 					   "",
 					   "",
 					   IPMI_AUTHENTICATION_TYPE_NONE,
@@ -31,11 +90,12 @@ void ApolloReader::Read(){
 
   if (connection < 0){
     printf("bad connection\n");
-    throw std::runtime_error(strerror(ipmi_ctx_errnum(ipmiContext)));
+    throw std::runtime_error(strerror(ipmi_ctx_errnum(ipmiContext_)));
   }
 
+  ReadInformationLength(ipmiContext_);
+  ReadHeader(ipmiContext_);
 
-  
   uint8_t channel_number = 0;
   uint8_t lun = 0;
   uint8_t net_fn = 0xa; // 0xa is net fn for storage
@@ -56,15 +116,16 @@ void ApolloReader::Read(){
   buf_rq[4] = num_bytes_to_read;
 
   const size_t buf_rs_size = 256;
-
+  uint8_t buf_rs[buf_rs_size];
+  
   while (starting_byte_ls < 248){
 
-    printf("%03d       ", starting_byte_ls);
+    //printf("%03d       ", starting_byte_ls);
 
      
     int raw_result = ipmi_cmd_raw_ipmb(ipmiContext_,
 				       channel_number,
-				       0x88,
+				       deviceAccessAddress,
 				       lun,
 				       net_fn,
 				       (void const *) buf_rq, buf_rq_size,
@@ -78,21 +139,30 @@ void ApolloReader::Read(){
     if(starting_byte_ls == 0 && starting_byte_ms == 0){
 
       for(int i = 3; i < 3+num_bytes_to_read; i++){
-	printf("header[%d] = %d\n", i, buf_rs[i]);
+	//printf("header[%d] = %d\n", i, buf_rs[i]);
 	header.push_back(buf_rs[i]);
       }
     }
-    
+
+    starting_byte_ls += 8;
+    buf_rq[2] =  starting_byte_ls;
   }
+  headerFormatVersion = header[0];
+  internalUseStartingOffset = header[1]*8;
+  chassisInfoStartingOffset = header[2]*8;
+  boardStartingOffset = header[3]*8;
+  productInfoStartingOffset = header[4]*8;
+  multiRecordStartingOffset = header[5]*8;
+  pad = header[6];
+  commonHeaderChecksum = header[7];
+  int total_bytes_used = informationLength - commonHeaderChecksum;
 
   
 }
 
 
 std::vector<uint8_t> ApolloReader::GetHeader(){
-
-
-
+  return header;
 }
 
 
